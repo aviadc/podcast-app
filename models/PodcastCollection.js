54c73be516cd8cfa2f984157929f5da91ca9b935
aviadc <coaviadco@gmail.com>
2022-02-07 16:42:10 +0200

    ok

--- models/PodcastCollection.js
+++ models/PodcastCollection.js
@@ -4,8 +4,14 @@ const mongoose = require('mongoose');
 const PodcastCollectionSchema = new mongoose.Schema({
   image:{
       type: Buffer
+  },
+  podcasts:{
+    type: [{name: String,audio: String,image: Buffer}],
+  },
+  title: {
+    type: String,
+    required: true,
   }
-
 });
 
 module.exports = mongoose.model('PodcastCollection',PodcastCollectionSchema);
