24e35b0266913f2f8e606cd24ac8fe586787731b
aviadc <coaviadco@gmail.com>
2022-05-23 10:30:36 +0300

    make it better2

--- client/src/components/pages/Home/index.js
+++ client/src/components/pages/Home/index.js
@@ -9,8 +9,8 @@ const Home = () => {
       <div className='home-container'>
         <div className='home-inner'>
           <div className='home-inner-wrapper'>
-            <h3>welcome to podco!</h3>
-            <h3>in podco you can create</h3>
+            <h3>welcome to Podco!</h3>
+            <h3>in Podco you can create</h3>
             <h3>your own podcast</h3>
             <h3>channel for free!</h3>
           </div>
