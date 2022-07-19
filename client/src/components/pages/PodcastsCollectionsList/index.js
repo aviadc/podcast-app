f34e9d6db6da2ea24962a78a44549f5bf79a4e37
aviadc <coaviadco@gmail.com>
2022-07-19 14:23:13 +0300

    fhinish testing

--- client/src/components/pages/PodcastsCollectionsList/index.js
+++ client/src/components/pages/PodcastsCollectionsList/index.js
@@ -11,13 +11,13 @@ const PodcastCollectionsList = () => {
 
   useEffect(() => {
     async function innerUseEffect() {
-      console.log("im in podcasts page in use effect");
+      // console.log("im in podcasts page in use effect");
       const list = localStorage.getItem('collectionsList');
-      console.log("list row 16", list);
+      // console.log("list row 16", list);
       if (!list) {
         try {
           const { data } = await podcastApi.get('/podcasts');
-          console.log("data row 20", data);
+          // console.log("data row 20", data);
           localStorage.setItem('collectionsList', JSON.stringify(data));
           setCollectionsList(data);
         } catch (err) {
@@ -26,8 +26,8 @@ const PodcastCollectionsList = () => {
       } else {
         setCollectionsList(JSON.parse((localStorage.getItem('collectionsList'))));
       }
-      console.log("list row 29", list);
-      console.log("collection list row 30",collectionsList);
+      // console.log("list row 29", list);
+      // console.log("collection list row 30",collectionsList);
     }
     innerUseEffect();
   }, [])
@@ -38,8 +38,8 @@ const PodcastCollectionsList = () => {
       <div className='page-background-1'></div>
       <StyledContainer>
         <div className='collection-list-container'>
-          {console.log("collection list in block",collectionsList)}
-          {/* {!!collectionsList.length && collectionsList.map((collection) => {
+          {/* {console.log("collection list in block",collectionsList)} */}
+          {!!collectionsList.length && collectionsList.map((collection) => {
             return (
               <PodcastCollectionItem
                 key={collection._id}
@@ -50,7 +50,7 @@ const PodcastCollectionsList = () => {
                 profile={false}
               />
             )
-          })} */}
+          })}
         </div>
       </StyledContainer>
     </>
